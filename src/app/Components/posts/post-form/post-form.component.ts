23b649674d55ac2d8726c6f0d6568e0ac3ef1885
Diego Julian Rodriguez <141148033+keronidas@users.noreply.github.com>
2024-03-22 17:34:56 +0100

    hasta el TODO 13

--- src/app/Components/posts/post-form/post-form.component.ts
+++ src/app/Components/posts/post-form/post-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder } from '@angular/forms';
+import { FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PostDTO } from 'src/app/Models/post.dto';
 import { CategoryService } from 'src/app/Services/category.service';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
 import { PostService } from 'src/app/Services/post.service';
@@ -12,6 +13,20 @@ import { SharedService } from 'src/app/Services/shared.service';
   styleUrls: ['./post-form.component.scss'],
 })
 export class PostFormComponent implements OnInit {
+
+  registerPost: PostDTO;
+  titte!: string;
+  description!: string;
+  num_likes!: number;
+  num_dislikes!: number;
+  date!: Date;
+  isValidForm: boolean | null;
+  registerForm: FormGroup;
+  categories!: string[];
+
+
+
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private postService: PostService,
@@ -20,7 +35,15 @@ export class PostFormComponent implements OnInit {
     private sharedService: SharedService,
     private localStorageService: LocalStorageService,
     private categoryService: CategoryService
-  ) {}
-  // TODO 13
-  ngOnInit(): void {}
+  ) {
+    this.registerPost = new PostDTO("", "", 0, 0, new Date());
+    this.isValidForm = null;
+    this.registerForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.maxLength(55)]],
+      description: ['', [Validators.required, Validators.maxLength(255)]],
+      categories: ['', [Validators.required]],
+      date: ['', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]],
+    });
+  }  // TODO 13
+  ngOnInit(): void { }
 }
--- src/app/Components/posts/posts-list/posts-list.component.ts
+++ src/app/Components/posts/posts-list/posts-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PostDTO } from 'src/app/Models/post.dto';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
 import { PostService } from 'src/app/Services/post.service';
@@ -12,172 +12,63 @@ import { SharedService } from 'src/app/Services/shared.service';
   styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent {
-  post: PostDTO;
-  title: UntypedFormControl;
-  description: UntypedFormControl;
-  num_likes: UntypedFormControl;
-  num_dislikes: UntypedFormControl;
-  publication_date: UntypedFormControl;
+  posts!: PostDTO[];
 
-  postForm: UntypedFormGroup;
-  isValidForm: boolean | null;
-  private isUpdateMode: boolean;
-  private validRequest: boolean;
-  private postId: string | null;
   constructor(
     private postService: PostService,
     private router: Router,
     private localStorageService: LocalStorageService,
     private sharedService: SharedService
   ) {
-    // TODO 12 working on it!
-    this.isValidForm = null;
-    this.postId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.post = new PostDTO('', '', 0, 0, new Date());
-    this.isUpdateMode = false;
-    this.validRequest = false;
-
-    this.title = new UntypedFormControl(this.post.title, [
-      Validators.required,
-      Validators.maxLength(55),
-    ]);
-
-    this.description = new UntypedFormControl(this.post.description, [
-      Validators.required,
-      Validators.maxLength(255),
-    ]);
-
-    this.num_likes = new UntypedFormControl(this.post.num_likes, [
-      Validators.required,
-      Validators.maxLength(7),
-    ]);
-    this.num_dislikes = new UntypedFormControl(this.post.num_dislikes, [
-      Validators.required,
-      Validators.maxLength(7),
-    ]);
-    this.publication_date = new UntypedFormControl(this.post.publication_date, [
-      Validators.required
-    ]);
-
-    this.postForm = this.formBuilder.group({
-      title: this.title,
-      description: this.description,
-      num_likes: this.num_likes,
-      num_dislikes: this.num_dislikes,
-      publication_date: this.publication_date,
-    });
+    this.loadPost();
   }
 
-  async ngOnInit(): Promise<void> {
+  private async loadPost(): Promise<void> {
     let errorResponse: any;
-
-    // update
-    if (this.postId) {
-      this.isUpdateMode = true;
-      try {
-        this.post = await this.postService.getPostById(
-          this.postId
-        );
-
-        this.title.setValue(this.post.title);
-
-        this.description.setValue(this.post.description);
-
-        this.num_likes.setValue(this.post.num_likes);
-        this.num_dislikes.setValue(this.post.num_dislikes);
-        this.publication_date.setValue(this.post.publication_date);
-
-        this.postForm = this.formBuilder.group({
-          title: this.title,
-          description: this.description,
-          css_color: this.css_color,
-        });
-      } catch (error: any) {
-        errorResponse = error.error;
-        this.sharedService.errorLog(errorResponse);
-      }
-    }
-  }
-
-  private async editCategory(): Promise<boolean> {
-    let errorResponse: any;
-    let responseOK: boolean = false;
-    if (this.postId) {
-      const userId = this.localStorageService.get('user_id');
-      if (userId) {
-        this.category.userId = userId;
-        try {
-          await this.categoryService.updateCategory(
-            this.postId,
-            this.category
-          );
-          responseOK = true;
-        } catch (error: any) {
-          errorResponse = error.error;
-          this.sharedService.errorLog(errorResponse);
-        }
-
-        await this.sharedService.managementToast(
-          'categoryFeedback',
-          responseOK,
-          errorResponse
-        );
-
-        if (responseOK) {
-          this.router.navigateByUrl('categories');
-        }
-      }
-    }
-    return responseOK;
-  }
-
-  private async createCategory(): Promise<boolean> {
-    let errorResponse: any;
-    let responseOK: boolean = false;
-    const userId = this.localStorageService.get('user_id');
+    const userId = this.localStorageService.get('post_id');
     if (userId) {
-      this.category.userId = userId;
       try {
-        await this.categoryService.createCategory(this.category);
-        responseOK = true;
+        this.posts = await this.postService.getPostsByUserId(
+          userId
+        );
       } catch (error: any) {
         errorResponse = error.error;
         this.sharedService.errorLog(errorResponse);
       }
-
-      await this.sharedService.managementToast(
-        'categoryFeedback',
-        responseOK,
-        errorResponse
-      );
-
-      if (responseOK) {
-        this.router.navigateByUrl('categories');
-      }
     }
-
-    return responseOK;
   }
 
-  async saveCategory() {
-    this.isValidForm = false;
+  createPost(): void {
 
-    if (this.categoryForm.invalid) {
-      return;
-    }
+    this.router.navigateByUrl('/user/post/');
 
-    this.isValidForm = true;
-    this.category = this.categoryForm.value;
+  }
 
-    // TO DO 10 works!
+  updatePost(postId: string): void {
 
-    if (this.isUpdateMode) {
-      this.validRequest = await this.editCategory();
-    } else {
-      this.validRequest = await this.createCategory();
+    this.router.navigateByUrl('/user/post/' + postId);
+  }
+
+  async deletePost(categoryId: string): Promise<void> {
+    let errorResponse: any;
+
+    // show confirmation popup
+    let result = confirm(
+      'Confirm delete category with id: ' + categoryId + ' .'
+    );
+    if (result) {
+      try {
+        const rowsAffected = await this.postService.deletePost(
+          categoryId
+        );
+        if (rowsAffected.affected > 0) {
+ 
+          this.loadPost();
+        }
+      } catch (error: any) {
+        errorResponse = error.error;
+        this.sharedService.errorLog(errorResponse);
+      }
     }
   }
 }
-
-
-
