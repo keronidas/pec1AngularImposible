f18fe8583e42daab4572167a8ee68f3166add106
Diego Julian Rodriguez <141148033+keronidas@users.noreply.github.com>
2024-03-24 20:08:47 +0100

    Trabajo terminado

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PostDTO } from 'src/app/Models/post.dto';
+import { PostService } from 'src/app/Services/post.service';
+import { SharedService } from 'src/app/Services/shared.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  posts!: PostDTO[];
+  totalLikes: number = 0;
+  totalDislikes: number = 0;
+
+  constructor(
+    private service: PostService,
+    private sharedService: SharedService,
+  ) {
+    this.loadPosts().then(() => {
+      for (let post of this.posts) {
+        this.totalLikes += post.num_likes;
+        this.totalDislikes += post.num_dislikes;
+      }
+    });
+
+
+
+  }
+  private async loadPosts(): Promise<void> {
+
+    let errorResponse: any;
+    try {
+      this.posts = await this.service.getPosts();
+    } catch (error: any) {
+      errorResponse = error.error;
+      this.sharedService.errorLog(errorResponse);
+    }
+  }
 }
